f31ea9a63bfcf9f9850a2fd32fc02e95f57fd681
gubard <maksimovsergey08@gmail.com>
2025-11-27 09:44:03 +0200

    Add dependency registration and usage for `IManisValidator` in `SpravaServiceProvider`

--- Sprava/Services/SpravaServiceProvider.cs
+++ Sprava/Services/SpravaServiceProvider.cs
@@ -15,6 +15,7 @@ namespace Sprava.Services;
 [Import(typeof(IMelnikovServiceProvider))]
 [Singleton(typeof(MainViewModel))]
 [Transient(typeof(PaneViewModel))]
+[Transient(typeof(IManisValidator), typeof(ManisValidator))]
 [Singleton(typeof(NavigationBarViewModel))]
 [Transient(typeof(CreateUserViewModel), Factory = nameof(GetCreateUserViewModel))]
 [Transient(typeof(ManisServiceOptions), Factory = nameof(GetManisServiceOptions))]
@@ -29,8 +30,8 @@ public partial class SpravaServiceProvider : Inanna.Services.IServiceProvider
     }
 
     public static CreateUserViewModel GetCreateUserViewModel(IApplicationResourceService appResourceService,
-        IManisService manisService)
+        IManisService manisService, IManisValidator manisValidator)
     {
-        return new CreateUserViewModel(appResourceService.GetResource<string>("Lang.CreateUser"), manisService);
+        return new CreateUserViewModel(appResourceService.GetResource<string>("Lang.CreateUser"), manisService, manisValidator);
     }
 }
