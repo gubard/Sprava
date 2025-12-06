39a060d50604131c30360c0ae3acfe44799eb2c2
gubard <maksimovsergey08@gmail.com>
2025-12-06 18:14:28 +0200

    Integrate dependency injection with `DiHelper.ServiceProvider` in iOS project startup.

--- Sprava.iOS/Main.cs
+++ Sprava.iOS/Main.cs
@@ -1,3 +1,5 @@
+using Gaia.Helpers;
+using Sprava.Services;
 using UIKit;
 
 namespace Sprava.iOS;
@@ -7,8 +9,8 @@ public class Application
     // This is the main entry point of the application.
     static void Main(string[] args)
     {
-        // if you want to use a different Application Delegate class from "AppDelegate"
-        // you can specify it here.
+        
+        DiHelper.ServiceProvider = new SpravaServiceProvider();
         UIApplication.Main(args, null, typeof(AppDelegate));
     }
 }
