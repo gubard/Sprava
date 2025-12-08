a7933f70a938a7485ffbaa72e9e7aece6db86e40
gubard <maksimovsergey08@gmail.com>
2025-12-08 15:32:17 +0200

    Remove unused using directives from App.axaml.cs and AppDelegate.cs.

--- Sprava/App.axaml.cs
+++ Sprava/App.axaml.cs
@@ -2,7 +2,6 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Gaia.Helpers;
 using Inanna.Services;
-using Microsoft.EntityFrameworkCore;
 using Sprava.Ui;
 
 namespace Sprava;
