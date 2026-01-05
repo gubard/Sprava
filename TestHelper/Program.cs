bd57d565fcac36dc3984d896e6c4c78b79ac2187
gubard <maksimovsergey08@gmail.com>
2026-01-05 09:29:20 +0200

    Add TestHelper project for environment folder path testing

---
+++ TestHelper/Program.cs
@@ -0,0 +1,10 @@
+﻿using Gaia.Helpers;
+
+var folders = Enum.GetValues<Environment.SpecialFolder>();
+
+Console.WriteLine($"OS: {OsHelper.OsType}");
+
+foreach (var folder in folders)
+{
+    Console.WriteLine($"{folder}: {folder.GetPath()}");
+}
