db08356a784d6fc4dc0f660575b15c4a76337a52
Serhii Maksymov <maksimovsergey08@gmail.com>
2026-01-17 19:24:44 +0200

    Add localization and UI updates to `DeveloperView` with `TopLevel` type and classes display

--- Sprava/Ui/DeveloperViewModel.cs
+++ Sprava/Ui/DeveloperViewModel.cs
@@ -1,22 +1,32 @@
+using Avalonia.Collections;
 using Avalonia.Controls;
 using Avalonia.VisualTree;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Inanna.Helpers;
 using Inanna.Models;
+using Inanna.Services;
 
 namespace Sprava.Ui;
 
 public partial class DeveloperViewModel : ViewModelBase
 {
+    public IEnumerable<string> Classes => _classes;
+
+    private readonly AvaloniaList<string> _classes = new();
+
     [ObservableProperty]
     private MaterialDesignSizeType _designSizeType;
+
+    [ObservableProperty]
+    private Type? _topLevelType;
 
     [RelayCommand]
     private void Refresh(Button button)
     {
-        DesignSizeType = TopLevelAssist.GetMaterialDesignSizeType(
-            button.GetVisualAncestors().OfType<TopLevel>().First()
-        );
+        var topLevel = button.GetVisualAncestors().OfType<TopLevel>().First();
+        TopLevelType = topLevel.GetType();
+        _classes.UpdateOrder(topLevel.Classes.ToArray());
+        DesignSizeType = TopLevelAssist.GetMaterialDesignSizeType(topLevel);
     }
 }
