df2089a1bf798cf2624cd5ce832ee5dee0f0e153
gubard <maksimovsergey08@gmail.com>
2026-01-18 16:38:10 +0200

    Invoke `UpdateMaterialDesignSizeType` on MainView load.

--- Sprava/Ui/MainView.axaml.cs
+++ Sprava/Ui/MainView.axaml.cs
@@ -1,4 +1,7 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.VisualTree;
+using Inanna.Services;
 
 namespace Sprava.Ui;
 
@@ -7,5 +10,22 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+
+        Loaded += (_, e) =>
+        {
+            if (e.Source is not Visual visual)
+            {
+                return;
+            }
+
+            var topLevel = visual.GetVisualAncestors().OfType<TopLevel>().FirstOrDefault();
+
+            if (topLevel is null)
+            {
+                return;
+            }
+
+            InannaApplication.UpdateMaterialDesignSizeType(topLevel);
+        };
     }
 }
