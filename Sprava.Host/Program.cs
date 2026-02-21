f2e20119d5578c5f69cef45d9a0832991dffa5c1
gubard <maksimovsergey08@gmail.com>
2026-02-21 19:06:07 +0200

    Update static file handling to support custom '.dat' file MIME type

--- Sprava.Host/Program.cs
+++ Sprava.Host/Program.cs
@@ -1,5 +1,13 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-app.UseStaticFiles();
+
+var contentTypeProvider = new FileExtensionContentTypeProvider
+{
+    Mappings = { [".dat"] = "application/octet-stream" },
+};
+
+app.UseStaticFiles(new StaticFileOptions { ContentTypeProvider = contentTypeProvider });
 app.MapGet("/", () => Results.Redirect("/index.html", true));
 app.Run();
