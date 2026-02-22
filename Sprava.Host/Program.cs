8fbb3e4dd28c280d6dcc252d39a4da91be84efe5
gubard <maksimovsergey08@gmail.com>
2026-02-22 18:17:42 +0200

    Add new content type mappings for `.abb`, `.apk`, and `.flatpak` files

--- Sprava.Host/Program.cs
+++ Sprava.Host/Program.cs
@@ -5,7 +5,14 @@ var app = builder.Build();
 
 var contentTypeProvider = new FileExtensionContentTypeProvider
 {
-    Mappings = { [".dat"] = "application/octet-stream", [".dll"] = "application/octet-stream" },
+    Mappings =
+    {
+        [".dat"] = "application/octet-stream",
+        [".dll"] = "application/octet-stream",
+        [".abb"] = "application/octet-stream",
+        [".apk"] = "application/octet-stream",
+        [".flatpak"] = "application/octet-stream",
+    },
 };
 
 app.UseStaticFiles(new StaticFileOptions { ContentTypeProvider = contentTypeProvider });
